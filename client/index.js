b74fe1b2c22b4dff5e510508bedbd496beb7d30e
TiTo <yukkasource@gmail.com>
2015-07-26 20:07:58 +0200

    added some stuff

---
+++ client/index.js
@@ -0,0 +1,7 @@
+var $ = require('jquery');
+window.$ = $;
+window.jQuery = $;
+require("bootstrap");
+require("./variables.less");
+require("./bootswatch.less")
+require("./assets/js/bootswatch.js");
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	entry: [
+	"./client/index.js"
+	],
+	output: {
+		path: "./dist",
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+		//{ test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+		{ test: /\.html$/i, loader: "html" },
+		{ test: /\.css$/, loader: "style!css" },
+		{ test: /\.less$/i, loader: "style!css!less" },
+		{ test: /\.scss$/i, loader: "style!css!sass" },
+		{ test: /\.jpe?g$|\.gif$|\.eot(\?v=\d+\.\d+\.\d+)?$|\.png$|\.wav$|\.mp3$/,loader: "file" },
+		{ test: /\.woff[0-9]?(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+		{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+		{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
+
+		]
+	},
+	resolve: {
+		modulesDirectories: ['node_modules'],
+	},
+};
