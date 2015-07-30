ae17aa11bd628fe4719e02f7227e29149b6f4e8c
Potito Coluccelli <potito.coluccelli@lumatagroup.com>
2015-07-30 18:28:14 +0200

    wip: generating html

--- client/index.js
+++ client/index.js
@@ -4,3 +4,5 @@ window.jQuery = $;
 require("bootstrap");
 require("./main.less");
 require("./assets/js/bootswatch.js");
+require("file?name=[path][name].[ext]?[hash]!./assets/img/shopify.png")
+require("html!./index.html");
