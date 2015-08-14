65c2393390b14ce1854b5aa0ba61cd8db02bb04f
Potito Coluccelli <potito.coluccelli@lumatagroup.com>
2015-08-14 18:12:22 +0200

    fixed mockup and added a message in mocks data

--- client/components.jsx
+++ client/components.jsx
@@ -141,6 +141,12 @@ var MessagesList = React.createClass({
   }
 });
 
+
+/*
+*****************************************************************
+*                        Mock DATA
+*****************************************************************
+*/
 var MESSAGES = [
     {id:'1',text:'Hi, How are you?', attachments:[], user:'user1'},
     {
@@ -161,11 +167,35 @@ var MESSAGES = [
           }
       ], user:'user2'
     },
-    {id:'3',text:'Thanks for files', attachments:[], user:'user1'}
+    {id:'3',text:'Thanks for files', attachments:[], user:'user1'},
+    {
+      id:'4',
+      text:'Hi, Other',
+      attachments:[
+          {
+            id:'1',
+            filename:'File1',
+            type:'PDF',
+            size:'10M',
+          },
+          {
+            id:'2',
+            filename:'File2',
+            type:'JPG',
+            size:'1M',
+          }
+      ], user:'user2'
+    }
 ];
 
 var USER = 'user2';
 
+
+/*
+*****************************************************************
+*                        Rendering
+*****************************************************************
+*/
 React.render(<MessagesList messages={MESSAGES}/>,document.getElementById('messages'));
 
 
