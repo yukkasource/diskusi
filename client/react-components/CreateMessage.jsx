4d3606a035d967a81b4678258b9f6999feb2c15a
TiTo <yukkasource@gmail.com>
2015-08-30 21:26:30 +0200

    added flux

---
+++ client/react-components/CreateMessage.jsx
@@ -0,0 +1,87 @@
+let React = require('react');
+let $ = require('jquery');
+window.$ = $;
+window.jQuery = $;
+
+let UploadFilesTable = require('./filestable').UploadFilesTable;
+let NormalButton = require('./commons').NormalButton;
+let CloseButton = require('./commons').CloseButton;
+let TextArea = require('./commons').TextArea;
+let store = require('../store/mock-store');
+
+
+
+let CreateMessage = React.createClass({
+  getInitialState: function(){
+    return  {
+              newMessage:false,
+              attachFiles:false
+            };
+  },
+  handleNewMessage: function(){
+    this.setState(
+      {
+        newMessage:!this.state.newMessage,
+        attachFiles:false
+      });
+  },
+  handleAttachFiles: function(){
+    this.setState(
+      {
+        newMessage:this.state.newMessage,
+        attachFiles:true
+      });
+    
+  },
+  handleClose: function(){
+    this.setState(
+      {
+        newMessage:!this.state.newMessage,
+        attachFiles:false
+      });
+  },
+  render: function() {
+    if (this.state.newMessage){
+      let uploadForm = '';
+      if (this.state.attachFiles){
+        uploadForm = 
+        <div>
+          <UploadFilesTable files={store.getFiles()}/>
+          <div className="modal-footer"></div>
+        </div>
+      }
+      var formNewMessage = 
+        <div className="modal">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <CloseButton clickHandler={this.handleClose}/>
+                  <h4 className="modal-title">{'Send Message'}</h4>
+                </div>
+                <div className="modal-body">
+                  <p><TextArea id="message-text"/></p>
+                </div>
+                <div className="modal-footer">
+                  <NormalButton buttonclass={'btn-default'} label={'Attach Files...'} clickHandler={this.handleAttachFiles}  />
+                  <NormalButton label={'Send'} />
+                </div>
+                <div className="modal-body">
+                  <div className="bs-component">
+                    {uploadForm}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    }else{
+      var formNewMessage = <NormalButton label={'New Message...'} clickHandler={this.handleNewMessage} />
+    }
+    return(
+      <div className="bs-component">
+        {formNewMessage}
+      </div>
+      );
+  }
+});
+
+module.exports = CreateMessage;
